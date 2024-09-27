4c28a1bca8d739098afdbe8b727be6d1da8f0ccf
Vladimir <115186975+scobca@users.noreply.github.com>
2024-09-27 19:38:38 +0300

    Create task1.bash

---
+++ lab_0/task1.bash
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+cd ~
+mkdir lab0
+cd lab0/
+
+mkdir bibarel4
+cd bibarel4/
+
+mkdir anorith
+mkdir croagunk 
+
+touch archeops
+echo "Развитые способности Intimidate" > archeops
+
+touch dwebble
+echo "Возможности" >> dwebble
+echo "Overland=5 Surface=5 Underwater=3 Jump=1" >> dwebble
+echo "Intellidence=4" >> dwebble
+
+touch igglybuff
+echo "Способности Charm Sing Defence Curl Pound" >> igglybuff
+echo "Sweet Kiss Copycat" >> igglybuff
+
+mkdir archen
+
+cd ../
+
+touch bronzor6
+echo "Способности Unbreakable Mind Mold" >> bronzor6
+echo "Levitate Heatproof" >> bronzor6
+
+touch chikorita7
+echo "Ходы Ancientpower Bullet Seed" >> chikorita7
+echo "Double-Edge Fury Cutter Giga Drain Grass Pledge Iron Tail Magic Coat" >> chikorita7
+echo "Mud-Slap Role Play Secret Power Seed Bomb Sleep Talk Snore Synthesis" >> chikorita7
+echo "Worry Seed" >> chikorita7
+
+mkdir leavanny8
+cd leavanny8/
+
+touch lopunny
+echo "Тип диеты" >> lopunny
+echo "Omnivore" >> lopunny
+
+touch floatzel
+mkdir gabite
+mkdir lumineon
+
+cd ../
+
+touch skuntank8
+echo "satk=7 sdef=6 spd=8" >> skuntank8
+
+mkdir stoutland3
+cd stoutland3/
+mkdir cubchoo
+
+touch sharpedo
+echo "Живет Freshwater" >> sharpedo
+echo "Ocean" >> sharpedo
+
+mkdir mienfoo
