90a94c87488dbffcc8028614804bd942f02efbf7
Vladimir <115186975+scobca@users.noreply.github.com>
2024-10-07 15:31:35 +0300

    Add files via upload

---
+++ lab_0/task2.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+cd ~
+cd lab0/
+
+chmod u=rwx,g=wx,o=rx bibarel4/
+
+cd bibarel4/
+chmod 571 anorith
+chmod -wx-wx-wx croagunk
+chmod u=r archeops
+chmod u=rw,g=,o=r dwebble
+chmod 666 igglybuff
+chmod -wx-wxr-x archen
+
+cd ../
+chmod ug=,o=rw bronzor6 
+chmod u=rw,g=,o=r chikorita7
+chmod u=rx,g=rwx,o=rwx leavanny8
+
+cd leavanny8
+chmod 664 lopunny
+chmod u=rw,g=w,o=r floatzel
+chmod 500 gabite
+chmod u=rx,g=rwx,o=wx lumineon/
+
+cd ~/lab0/
+
+chmod u=rw,g=r,o= skuntank8
+chmod u=wx,go=x stoutland3/
+
+cd stoutland3/
+chmod 337 cubchoo
+chmod 046 sharpedo
+chmod u=wx,g=rwx,o=rx mienfoo
